6ecaed06f431064c46f23dc36cb2f701a49d63d8
kj_cho <kj_cho@idealidea.co.kr>
2020-06-03 15:10:04 +0900

    의견 입력 화면 그리기

---
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/EditReplyActivity.java
@@ -0,0 +1,26 @@
+package kr.co.tjoeun.colosseum_kotlin;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class EditReplyActivity extends BaseActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_edit_reply);
+        setupEvents();
+        setValues();
+    }
+
+    @Override
+    public void setupEvents() {
+
+    }
+
+    @Override
+    public void setValues() {
+
+    }
+}
