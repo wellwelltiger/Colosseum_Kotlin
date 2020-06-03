dc1a1a6de5b7fbc370e76e6dc08b0733b9c7f6b7
kj_cho <kj_cho@idealidea.co.kr>
2020-06-03 10:14:22 +0900

    주제id만 전달받아서 상세정보를 서버에서 재확인

--- app/src/main/java/kr/co/tjoeun/colosseum_kotlin/ViewTopicActivity.java
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/ViewTopicActivity.java
@@ -4,8 +4,13 @@ import androidx.appcompat.app.AppCompatActivity;
 import androidx.databinding.DataBindingUtil;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.Toast;
+
+import org.json.JSONObject;
 
 import kr.co.tjoeun.colosseum_kotlin.databinding.ActivityViewTopicBinding;
+import kr.co.tjoeun.colosseum_kotlin.utils.ServerUtil;
 
 public class ViewTopicActivity extends BaseActivity {
 
@@ -26,6 +31,21 @@ public class ViewTopicActivity extends BaseActivity {
 
     @Override
     public void setValues() {
+
+        int topicId = getIntent().getIntExtra("topic_id", -1);
+
+        if (topicId == -1) {
+//            TODO - 주제가 제대로 못넘어왔다
+            Toast.makeText(mContext, "잘못된 접근입니다.", Toast.LENGTH_SHORT).show();
+            finish();
+        }
+
+        ServerUtil.getRequestTopicById(mContext, topicId, new ServerUtil.JsonResponseHandler() {
+            @Override
+            public void onResponse(JSONObject json) {
+                Log.d("토픽 상세정보", json.toString());
+            }
+        });
 
     }
 }
