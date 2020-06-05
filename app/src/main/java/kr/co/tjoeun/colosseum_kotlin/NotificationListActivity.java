0769808c4dd50301152100dcf25445eb80a36476
wellwelltiger <59465215+wellwelltiger@users.noreply.github.com>
2020-06-05 17:48:13 +0900

    서버에서 알림 목록을 받아와서 ArrayList에 저장하는 코드

--- app/src/main/java/kr/co/tjoeun/colosseum_kotlin/NotificationListActivity.java
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/NotificationListActivity.java
@@ -3,9 +3,22 @@ package kr.co.tjoeun.colosseum_kotlin;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import kr.co.tjoeun.colosseum_kotlin.datas.Notification;
+import kr.co.tjoeun.colosseum_kotlin.utils.ServerUtil;
+
 public class NotificationListActivity extends BaseActivity {
+
+    List<Notification> notificationList = new ArrayList<>();
 
 
     @Override
@@ -25,6 +38,41 @@ public class NotificationListActivity extends BaseActivity {
     public void setValues() {
 
         notificationImg.setVisibility(View.INVISIBLE);
+
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        getNotiFromServer();
+    }
+
+    void getNotiFromServer(){
+
+        ServerUtil.getRequestNotification(mContext, new ServerUtil.JsonResponseHandler() {
+            @Override
+            public void onResponse(JSONObject json) {
+                Log.d("알림 목록 응답",json.toString());
+
+                try {
+                    JSONObject data = json.getJSONObject("data");
+                    JSONArray notis = data.getJSONArray("notifications");
+
+                    notificationList.clear();
+
+                    for(int i = 0; i< notis.length(); i++){
+                        JSONObject notiObj = notis.getJSONObject(i);
+                        notificationList.add(Notification.getNotiFromJson(notiObj));
+                    }
+
+                    // notifyDataSetChange
+
+                } catch (JSONException e) {
+                    e.printStackTrace();
+                }
+
+            }
+        });
 
     }
 
