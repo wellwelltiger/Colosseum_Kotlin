3cb655bb93deaf76dad802b238a99d0b1937a7cc
wellwelltiger <59465215+wellwelltiger@users.noreply.github.com>
2020-06-05 17:21:45 +0900

    알림 어댑터 뼈대 작업

---
+++ app/src/main/java/kr/co/tjoeun/colosseum_kotlin/adapters/NotificationAdapter.java
@@ -0,0 +1,56 @@
+package kr.co.tjoeun.colosseum_kotlin.adapters;
+
+import android.content.Context;
+import android.content.Intent;
+import android.graphics.Color;
+import android.os.Handler;
+import android.os.Looper;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.List;
+
+import kr.co.tjoeun.colosseum_kotlin.R;
+import kr.co.tjoeun.colosseum_kotlin.ViewReplyActivity;
+import kr.co.tjoeun.colosseum_kotlin.datas.Notification;
+import kr.co.tjoeun.colosseum_kotlin.datas.TopicReply;
+import kr.co.tjoeun.colosseum_kotlin.utils.ServerUtil;
+
+public class NotificationAdapter extends ArrayAdapter<Notification> {
+
+
+    Context mContext;
+    List<Notification> mList;
+    LayoutInflater inf;
+
+    public NotificationAdapter(@NonNull Context context, int resource, @NonNull List<Notification> objects) {
+        super(context, resource, objects);
+        mContext = context;
+        mList = objects;
+        inf = LayoutInflater.from(mContext);
+    }
+
+    @NonNull
+    @Override
+    public View getView(int position, @Nullable View convertView, @NonNull ViewGroup parent) {
+        View row = convertView;
+
+        if (row == null) {
+            row = inf.inflate(R.layout.topic_reply_list_item, null);
+        }
+
+
+        return row;
+    }
+}
